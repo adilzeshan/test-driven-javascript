ce9a248feb2d7620feb2a8d45e9bd1cf4f1dcc54
Adil Zeshan <adil@zeshan.org>
2018-06-13 23:05:25 +0100

    Array #mySome(): pass in ith position as argument to the callback

--- test/arrays/iterators/mySome.test.js
+++ test/arrays/iterators/mySome.test.js
@@ -25,5 +25,11 @@ describe('Array #mySome()', () => {
         assert.strictEqual(element, 'item');
       });
     });
+
+    it('should pass in ith position as second argument to callback', () => {
+      ['item'].mySome(function callback(element, index) {
+        assert.strictEqual(index, 0);
+      });
+    });
   });
 });
